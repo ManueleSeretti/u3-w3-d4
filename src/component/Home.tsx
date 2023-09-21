0827f2611c2d0e42b774bdc5eaea99aec927c0cb
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-09-21 16:45:47 +0200

    ...

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row, Spinner } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { Article } from "../interfaces/interfaces";
 import SingleArticle from "./SingleArticle";
@@ -6,14 +6,17 @@ import SingleArticle from "./SingleArticle";
 const Home = () => {
   const endPoint = "https://api.spaceflightnewsapi.net/v4/articles";
   const [news, setNews] = useState<Article[]>([]);
+  const [load, setLoad] = useState(false);
 
   const fetchAllArticle = async () => {
+    setLoad(true);
     try {
       const resp = await fetch(endPoint);
       if (resp.ok) {
         const data = await resp.json();
         const articoli = data.results;
         setNews(articoli);
+        setLoad(false);
       }
     } catch (error) {
       console.log(error);
@@ -26,13 +29,19 @@ const Home = () => {
   return (
     <Container fluid className="bg-warning bg-opacity-25 ">
       <h1 className="text-danger">Latest Spaceflight news</h1>
-      <Row className="gy-3 mt-5">
-        {news.map((n) => (
-          <Col xs={3} key={`id-${n.id}`}>
-            <SingleArticle article={n} />
-          </Col>
-        ))}
-      </Row>
+      {load ? (
+        <Spinner className="mt-5" animation="border" role="status" variant="success">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <Row className="gy-3 mt-5">
+          {news.map((n) => (
+            <Col xs={3} key={`id-${n.id}`}>
+              <SingleArticle article={n} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 };
